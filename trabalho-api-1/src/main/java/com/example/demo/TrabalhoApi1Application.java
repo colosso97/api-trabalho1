5e8a01fdbf76726fcb62804bd58b56fe08034701
colosso97 <marcelovidal.ml@gmail.com>
2025-05-22 20:45:45 -0300

    create: first commit

---
+++ trabalho-api-1/src/main/java/com/example/demo/TrabalhoApi1Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrabalhoApi1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrabalhoApi1Application.class, args);
+	}
+
+}
