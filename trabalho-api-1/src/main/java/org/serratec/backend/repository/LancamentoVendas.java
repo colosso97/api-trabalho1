221ae203dd94bff6e285d1009e21fec2f21289d5
EltonKasesky <eltonkasesky9@gmail.com>
2025-05-23 12:46:52 -0300

    Finish vendedorAutonomo service and controller

--- trabalho-api-1/src/main/java/org/serratec/backend/repository/LancamentoVendas.java
+++
@@ -1,7 +0,0 @@
-package org.serratec.backend.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface LancamentoVendas extends JpaRepository<LancamentoVendas, Long> {
-
-}
--- trabalho-api-1/src/main/java/org/serratec/backend/repository/LancamentoVendasRepository.java
+++ trabalho-api-1/src/main/java/org/serratec/backend/repository/LancamentoVendasRepository.java
@@ -1,10 +1,10 @@
 package org.serratec.backend.repository;
 
-import org.serratec.backend.entity.VendedorEmpresa;
+import org.serratec.backend.entity.LancamentoVendas;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface VendedorEmpresaRepository extends JpaRepository<VendedorEmpresa, Long> {
+public interface LancamentoVendasRepository extends JpaRepository<LancamentoVendas, Long> {
 
 }
